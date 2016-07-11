3295ed4e4391f5bec2e9c6d5741ce1c137082bae
Ralph Cowling <speak@ralph-cowling.com>
2016-07-11 16:10:20 +0100

    Add mock endpoint for evw-number/dob validation
    - Just adding some dummy cases with a tendency to make things more like to be valid than not

---
+++ mocks/post/validate.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const validate = (body) => {
+  let num = body['evw-number'];
+  if(num === '1000INVALID') {
+    return {
+      error: 'CASE_NOT_FOUND'
+    };
+  }
+  if(num === 'TOOLATEM8') {
+    return {
+      error: 'CASE_NOT_UPDATABLE'
+    };
+  }
+  return {
+    success: true
+  };
+};
+
+const verify = {
+
+  cache: false,
+
+  path: '/verify/evw/',
+
+  template: function (params, query, body) {
+    return validate(body);
+  }
+};
+
+module.exports = verify;

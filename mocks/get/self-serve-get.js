12d0100c1dbc6f523d13b0ac663f4f804e62ab78
Paul D. Waite <paul@pauldwaite.co.uk>
2018-10-31 10:16:52 +0000

    DPS-1639: WIP: added mocks for new Self Serve API in Integration Service.

---
+++ mocks/get/self-serve-get.js
@@ -0,0 +1,44 @@
+// Get EVW details
+
+'use strict';
+
+const evwDetails = require('../templates/evw/evw-self-serve-get');
+
+const errorCase = function (num, token) {
+  let errmap = {
+    'expired': 'EXPIRED_TOKEN',
+    'invalid': 'INVALID_TOKEN',
+    'not-found': 'CASE_NOT_FOUND',
+    'not-ready': 'DOWNLOAD_NOT_READY'
+  };
+
+  if(token in errmap) {
+    return {
+      'error': errmap[token]
+    };
+  }
+  else if (num in errmap) {
+    return {
+      'error': errmap[num]
+    };
+  }
+  else {
+    return false;
+  }
+
+
+};
+
+const check = {
+
+  path: '/self-serve/get/:number/:token',
+
+  template: function (params) {
+    const num = params.number;
+    const token = params.token;
+
+    return errorCase(num, token) || evwDetails;
+  }
+};
+
+module.exports = check;
---
+++ mocks/templates/evw/evw-self-serve-get.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "name": "ROCHELLE STRAMIELLO",
+    "updateCount": 2,
+    "updateLimit": 2,
+    "arrival": {
+        "arrivalTravel": "Plane",
+        "arrivalDate": "2019-07-22",
+        "arrivalTime": "07:08",
+        "departureForUKDate": "2015-12-02",
+        "inwardDepartureCountry": "KEN",
+        "inwardDeparturePort": "London - Heathrow, LHR",
+        "portOfArrival": "London - Heathrow, LHR",
+        "flightDetailsCheck": "No"
+    },
+    "departure": {
+        // "knowDepartureDetails": "No",
+        // "ukDuration": "1 - 3 months"
+        "knowDepartureDetails": "Yes",
+        "departureTravel": "BA002",
+        "departureDate": "2016-02-01",
+        "portOfDeparture": "Baltic Wharf, Rochford, LON"
+    },
+    "accommodation": {
+        "ukAddress": [
+            "My House",
+            "UkAddress2",
+            "HellO!",
+            "UkA333sssss4",
+            "UkAd222ress5"
+        ],
+        "ukVisitPhoneNumber": "4827489422232"
+    }
+};
